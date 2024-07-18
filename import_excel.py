d5ca441f1c30f4f85be5ed8a622f6b40c7a038b5
ChookPC <ckianhoong@yahoo.com>
2024-07-18 23:00:06 +0200

    Uploaded excel file and filter data

--- import_excel.py
+++ import_excel.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import sqlite3
 
-def import_data_from_excel(Inventory for.xlsx)
+def import_data_from_excel(df):
     # 读取Excel文件
-    df = pd.read_excel(Inventory for.xlsx)
+    df = pd.read_excel('Inventory.xlsx')
+    df = df[df['SKU'] != 'Empty']
     # 连接到SQLite数据库
     conn = sqlite3.connect('warehouse.db')
     cursor = conn.cursor()
@@ -11,11 +12,11 @@ def import_data_from_excel(Inventory for.xlsx)
     # 遍历DataFrame并插入数据到数据库
     for _, row in df.iterrows():
         cursor.execute("INSERT INTO inventory (sku, sn_number, position, quantity, date) VALUES (?, ?, ?, ?, ?)", 
-                       (row['SKU'], row['SN Number'], row['Position'], row['Quantity'], row['Date']))
+                       (row['SKU'], row['SN'], row['Position'], row['Quantity']))
     
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
     file_path = 'Inventory for.xlsx'  # 将此路径替换为您的Excel文件路径
-    import_data_from_excel(Inventory for.xlsx)
+    import_data_from_excel('Inventory.xlsx')
